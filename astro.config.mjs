076bf96440e8f28e5f66d31eee1d74d8fd4677ec
michalvarys <info@michalvarys.eu>
2026-02-15 19:57:49 +0100

    deploy

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://michalvarys.github.io',
+  base: '/vonavky/',
+  compressHTML: true,
+});
